5623bdd4b3b7b568536651baec6fb31a8ede0d8b
Semigroup <akin10@t-online.de>
2016-05-24 20:56:16 +0200

    bfbv

---
+++ Software/Werwolf/Werwolf/Inhalt/Element.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Werwolf.Inhalt
+{
+    public abstract class Element
+    {
+        /// <summary>
+        /// Mit Underlines statt Whitespaces
+        /// </summary>
+        public string Name { get; private set; }
+        public string Schreibname
+        {
+            get
+            {
+                return Name.Replace('_', ' ');
+            }
+        }
+        public string Desc { get; private set; }
+        public Image Bild { get; private set; }
+    }
+}
---
+++ Software/Werwolf/Werwolf/Karten/Karte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Werwolf.Karten
+{
+    public abstract class Karte
+    {
+        /// <summary>
+        /// Größe in Millimeter
+        /// </summary>
+        public SizeF Size { get; private set; }
+
+
+    }
+}
