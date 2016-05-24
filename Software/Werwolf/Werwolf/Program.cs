e0101b9296ebd11c4b4624646e64b4d562615a03
Semigroup <akin10@t-online.de>
2016-05-24 20:37:46 +0200

    Init

---
+++ Software/Werwolf/Werwolf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Werwolf
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+        }
+    }
+}
